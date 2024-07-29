7cf7d23ef5f06a9e8bec27e9996705d1dd8fd048
rimsiw <rimwitkowska@proton.me>
2024-07-30 01:02:51 +0200

    updated link

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,9 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'Linux Tools Wiki',
+			title: 'Linux Wiki',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/rimsiw/rims-linux-wiki',
 			},
 			sidebar: [
 				{
