9c0372bb5cded329e5a09d788e2f3c539a99f26a
rimsiw <rimwitkowska@proton.me>
2024-07-30 00:29:35 +0200

    Initial commit 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Linux Tools Wiki',
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
@@ -14,7 +14,14 @@ export default defineConfig({
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Start Page ', slug: 'guides/start' },
+						{ label: 'Distro List', slug: 'guides/distro' },
+						{ label: 'Internet Tools', slug: 'guides/internet' },
+						{ label: 'Multimedia Tools', slug: 'guides/multimedia' },
+						{ label: 'System Tools', slug: 'guides/system' },
+						{ label: 'Ricing Tools', slug: 'guides/ricing' },
+						{ label: 'Gaming Tools', slug: 'guides/gaming' },
+						{ label: 'Development Tools', slug: 'guides/development' },
 					],
 				},
 				{
---
+++ src/content/docs/guides/start.md
@@ -0,0 +1,22 @@
+---
+title: Start Page
+description: General information.
+---
+
+## Icon Definition List
+
+<details> <summary>Click to expand</summary>
+
+    ⭐ - General recommendations; best software in its kind.
+    ⚠️ - Be cautious while using; non-FOSS software.
+    🖥️ - CLI tool.
+
+</details>
+
+## Glossary
+
+<details> <summary>Click to expand</summary>
+
+    Nightly build - A beta/developer version of software.
+
+</details>
